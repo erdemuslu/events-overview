93e2401beb34f9a9c4f98e3dbbd6090df9a132df
Erdem Uslu <usluerdem@yandex.com>
2024-04-20 09:13:26 +0300

    feat(main-statistics): add an example data

---
+++ src/constants/main-statistics.js
@@ -0,0 +1,62 @@
+export const mainBoxInfo = [
+  {
+    label: "Total event count",
+    type: "total-event",
+    info: "lorem ipsum dolor sit amet",
+  },
+  {
+    label: "Events per user",
+    type: "events-per-user",
+    info: "lorem ipsum dolor sit amet",
+  },
+  {
+    label: "Events per session",
+    type: "events-per-session",
+    info: "lorem ipsum dolor sit amet",
+  },
+];
+
+export const mainBoxData = {
+  "total-event": {
+    "last-30-days": {
+      value: "349,260",
+      summaryPercantageValue: 30.3,
+    },
+    "last-week": {
+      value: "452,124",
+      summaryPercantageValue: 30.3,
+    },
+    "last-year": {
+      value: "6523,154",
+      summaryPercantageValue: 30.3,
+    },
+  },
+  "events-per-user": {
+    "last-30-days": {
+      value: "247,9",
+      summaryPercantageValue: 22.2,
+    },
+    "last-week": {
+      value: "452,124",
+      summaryPercantageValue: 30.3,
+    },
+    "last-year": {
+      value: "6523,154",
+      summaryPercantageValue: 30.3,
+    },
+  },
+  "events-per-sessions": {
+    "last-30-days": {
+      value: "32,5",
+      summaryPercantageValue: 4.5,
+    },
+    "last-week": {
+      value: "452,124",
+      summaryPercantageValue: 30.3,
+    },
+    "last-year": {
+      value: "6523,154",
+      summaryPercantageValue: 30.3,
+    },
+  },
+};
