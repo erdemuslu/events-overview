db84faf257a96aae03469f7f62768684be2a1875
Erdem Uslu <usluerdem@yandex.com>
2024-04-21 17:09:13 +0300

    feat: add date-picker

--- src/sections/monitor-events/monitor-events.jsx
+++ src/sections/monitor-events/monitor-events.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import "./monitor-events.css";
 
 import { Button, Box, InfoView } from "../../components";
-
 import { monitorEventsData } from "../../constants/monitor-events";
 
 const MonitorEvents = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+
   return (
     <div className="monitor-events">
       <div className="monitor-events-header">
@@ -13,6 +24,15 @@ const MonitorEvents = () => {
           <InfoView desc="lorem ipsum dolor sit amet" />
         </div>
         <div className="monitor-events-actions">
+          <div className="monitor-events-date">
+            <ReactDatePicker
+              selected={startDate}
+              onChange={onChange}
+              startDate={startDate}
+              endDate={endDate}
+              selectsRange
+            />
+          </div>
           <Button>Configure events</Button>
         </div>
       </div>
